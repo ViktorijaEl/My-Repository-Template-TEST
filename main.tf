395d35d9a837173dfe86f6110a345f79d70f40c5
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-29 19:03:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,7 @@ terraform {
 }
 
 resource "aws_instance" "example" {
-  # Your resource configuration
+  ami           = "ami-12345678"        # Replace with the desired AMI ID
+  instance_type = "t2.micro"            # Replace with the desired instance type
+  # Additional configuration for the instance
 }
