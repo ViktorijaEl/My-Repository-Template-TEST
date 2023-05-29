3f284bf10ca0d29138bc794618fc7e9ed5a97f24
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-29 19:02:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-# main.tf
-
-provider "aws" {
-  region = "us-west-2"
+terraform {
+  required_providers {
+    aws = ">= 3.0"
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
+  # Your resource configuration
 }
