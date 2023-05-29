8eb0decaf165e7d1aa600b6d3cdc88472f07ade9
ViktorijaEl <93345001+ViktorijaEl@users.noreply.github.com>
2023-05-29 19:55:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# terraform {
+#   required_providers {
+#     aws = ">= 3.0"
+#   }
+# }
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c94855ba95c71c99"  # Replace with the actual AMI ID
+#   instance_type = "t2.micro"               # Replace with the desired instance type
+#   # Additional configuration for the instance
+# }
 terraform {
   required_providers {
     aws = ">= 3.0"
@@ -5,7 +16,24 @@ terraform {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  # Replace with the actual AMI ID
-  instance_type = "t2.micro"               # Replace with the desired instance type
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+
+  monitoring {
+    enabled = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
+  ebs_optimized = true
+
+  iam_instance_profile = "your-iam-role"
+
   # Additional configuration for the instance
 }
